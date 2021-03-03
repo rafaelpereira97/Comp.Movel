58549c8355ff88fbde13ae4822b30989a34fabe6
Rafael <rafaelpereira_97@outlook.com>
2021-03-03 23:58:44 +0000

    Alteração do codigo para ser possivel depois adicionar marcadores vindos da API.

--- app/src/main/java/com/example/compmovel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/compmovel/ui/home/HomeFragment.kt
@@ -1,23 +1,22 @@
 package com.example.compmovel.ui.home
 
-import android.annotation.SuppressLint
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.compmovel.R
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
@@ -46,16 +45,10 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 
 
     override fun onMapReady(map: GoogleMap?) {
-        map?.apply {
-            val viana = LatLng(41.691807, -8.834451)
-            addMarker(
-                MarkerOptions()
-                    .position(viana)
-                    .title("Viana centro")
-                    .snippet("Buraco muito fundo na estrada !")
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.warning))
-            )
 
+        map?.apply {
+
+            createMarker(41.691807,-8.834451,"Viana","Cuidado! Buraco muito fundo na estrada !",map)
 
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(41.691807, -8.834451), 12f))
 
@@ -70,4 +63,22 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             }
         }
     }
+
+    protected fun createMarker(
+        latitude: Double,
+        longitude: Double,
+        title: String?,
+        snippet: String?,
+        map: GoogleMap?
+    ): Marker? {
+        val marker = map?.addMarker(
+                MarkerOptions()
+                    .position(LatLng(latitude,longitude))
+                    .title(title)
+                    .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.warning))
+                )
+        return marker
+    }
+
 }
